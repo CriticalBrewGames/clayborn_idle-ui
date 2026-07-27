8167f3af1934daf6eea933233dbd9ab90d70da09
Lampert Milan <lamp.milan@gmail.com>
2026-07-27 08:56:02 +0200

    Empty Host panesl removed, replaced with generic Wyvenbox Template scene, that is support dynamic injection

--- singleton/theme_updater.gd
+++ singleton/theme_updater.gd
@@ -1,6 +1,6 @@
 extends UIUpdaterBreakpoint
 
-@onready var main_theme: Theme = preload("res://ui/assets/theme_test.tres")
+@onready var main_theme: Theme = preload("res://assets/theme_test.tres")
 
 func _update_gui_bp(new_breakpoint: BreakpointsSchemas.Breakpoint) -> void:
 	main_theme.set_font_size("font_size", "Label", ScaleRuleBook.DEFAULT_FONT_SCALES[new_breakpoint])
