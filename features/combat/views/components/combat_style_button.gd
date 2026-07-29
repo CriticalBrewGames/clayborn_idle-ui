9a4e09ee50c10b17c90b0a667fed4dee9d99b49c
Lampert Milan <lamp.milan@gmail.com>
2026-07-29 08:11:15 +0200

    changed regualr buttons to payload buttons

--- features/combat/views/components/combat_style_button.gd
+++ features/combat/views/components/combat_style_button.gd
@@ -1,26 +1,23 @@
-extends Button
+extends PayloadButton
 
-enum WeaponType { ATTACK, STR, DEFENCE, AGILITY }
+enum AttackType { ATTACK, STR, DEFENCE, AGILITY }
 
 const COMBAT_MAPPING = {
-	WeaponType.ATTACK: preload("res://assets/skill_icons/attack.svg"),
-	WeaponType.STR: preload("res://assets/skill_icons/strength.svg"),
-	WeaponType.DEFENCE: preload("res://assets/skill_icons/defence.svg"),
-	WeaponType.AGILITY: preload("res://assets/skill_icons/agility.svg"),
+	AttackType.ATTACK: preload("res://assets/skill_icons/attack.svg"),
+	AttackType.STR: preload("res://assets/skill_icons/strength.svg"),
+	AttackType.DEFENCE: preload("res://assets/skill_icons/defence.svg"),
+	AttackType.AGILITY: preload("res://assets/skill_icons/agility.svg"),
 }
-
-@export var attack_type: WeaponType
-## Payload matches Core DamageType / DefenseType enum ints.
-@export var payload: int
 
 @onready var combat_icon: TextureRect = $TextureRect
 
+@export var attack_type: AttackType
 
 func _ready() -> void:
 	_set_icon_content(attack_type)
 
 
-func _set_icon_content(to_get: WeaponType) -> void:
+func _set_icon_content(to_get: AttackType) -> void:
 	var texture: Texture2D = COMBAT_MAPPING.get(to_get)
 	if texture == null or combat_icon == null:
 		return
